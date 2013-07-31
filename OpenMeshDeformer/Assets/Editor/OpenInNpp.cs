dd6738b80edd2b9cd9699a9261a507b8c9890f44
mp4 <therealmarshpoulson@gmail.com>
2013-07-31 12:19:25 -0600

    removed unneeded shaders added seamless shaders added starting npp support for opening shaders from unity

---
+++ OpenMeshDeformer/Assets/Editor/OpenInNpp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Diagnostics;
+
+[CustomEditor(typeof(ShaderImporter))]
+public class OpenInNpp : Editor {
+	Process x = null;
+	void appQuit(System.Object sender, EventArgs o)
+	{
+		x = null;
+		//UnityEngine.Debug.Log("setting proccess to null since it has exited");
+	}
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+		DrawDefaultInspector();
+		string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+		if(GUILayout.Button("open in Npp"))
+		{
+			//UnityEngine.Debug.Log("open in Npp pressed" + path);
+			try
+			{
+				if(x == null)
+				{
+					x = new Process();
+					x.StartInfo.FileName = "notepad++.exe";// + " -lShaderLab d";
+					x.StartInfo.Arguments = path;
+					x.Exited += appQuit;
+					x.Start();
+				}
+			}
+			catch(Exception e)
+			{
+				//UnityEngine.Debug.LogException(e);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ UnityOpenMeshDeformer
 a set of scripts designed to aid in deforming unity 3d meshes on the fly
 includes a library licensed under the LGPL all other code is under the 
 mit license and may be used for any purpose
-includes all of the builtin shaders in the shaders folder which are property of Unity Technologies
+includes shaders for seamless rendering
